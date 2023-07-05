e7451934326902d91c1fa4da3883780e17f2ec47
simon <ghdud0503@gmail.com>
2023-07-05 23:12:52 +0900

    sequelize 연결 추기

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const models = require('./models/index.js');
+
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// DB 연결 확인
+models.sequelize
+  .sync()
+  .then(() => {
+    console.log(' DB 연결 성공');
+  })
+  .catch((err) => {
+    console.log('연결 실패');
+    console.log(err);
+  });
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
